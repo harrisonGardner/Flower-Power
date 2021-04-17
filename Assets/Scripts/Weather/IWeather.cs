8c1f1d260ac4b653ca9ba3b3c174cd82bb724944
gliserma <64933173+gliserma@users.noreply.github.com>
2021-04-17 15:33:05 -0400

    Finished Weather Classes

--- Assets/Scripts/Weather/IWeather.cs
+++ Assets/Scripts/Weather/IWeather.cs
@@ -2,11 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum WeatherType { rain, sun, wind }
-
 public interface IWeather
 {
     public WeatherType Type { get; set; }
 
-    public void setDaysWeather(Garden garden);
+    public void SetDaysWeather(Garden garden);
 }
