3a46a09fcf559f605cc326977a0540201be2edf1
harrisonGardner <harrisongardner531@gmail.com>
2021-04-21 12:44:15 -0600

    Particle System Stuff

--- Assets/Scripts/Gardens/Plots/PlotSpriteUpdater.cs
+++ Assets/Scripts/Gardens/Plots/PlotSpriteUpdater.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class PlotSpriteUpdater : MonoBehaviour, ISpriteUpdate
 {
+    public ParticleSystem pollenParticlesPrefab;
+    private ParticleSystem pollenParticles;
+
+    private void Start()
+    {
+        pollenParticles = Instantiate(pollenParticlesPrefab, gameObject.transform);
+        pollenParticles.transform.parent = gameObject.transform;
+        var main = pollenParticles.main;
+        main.startColor = new UnityEngine.Color(1, 1, 1, 1);
+        var colGrad = pollenParticles.colorOverLifetime;
+        Gradient grad = new Gradient();
+        grad.SetKeys(new GradientColorKey[] { new GradientColorKey((UnityEngine.Color.blue + UnityEngine.Color.red) / 2, 0.0f),
+            new GradientColorKey((UnityEngine.Color.blue + UnityEngine.Color.red) / 2, 1.0f) }, new GradientAlphaKey[] { new GradientAlphaKey(0.0f, 0.0f),
+                new GradientAlphaKey(1.0f, 0.5f), new GradientAlphaKey(0.0f, 1.0f) });
+        colGrad.color = grad;
+    }
+
+    private void Update()
+    {
+        
+    }
+
     public void SpriteUpdate()
     {
         if (gameObject.GetComponent<Plot>().waterLevel <= 0)
