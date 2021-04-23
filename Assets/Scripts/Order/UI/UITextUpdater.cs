d8fdd48f24a3fae9b315b5bae51c2a7fc7e0b9f3
harrisonGardner <harrisongardner531@gmail.com>
2021-04-22 20:27:25 -0600

    Order Number UI updates I think
    Hard to say if there aren't orders I guess.
    Called when clippers are used

---
+++ Assets/Scripts/Order/UI/UITextUpdater.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITextUpdater : MonoBehaviour
+{
+    public static void UpdateOrderNumbers()
+    {
+        GameObject orderNumberObject = GameObject.Find("OrderNumberHolder");
+        foreach (Transform child in orderNumberObject.transform)
+        {
+            child.GetComponent<OrderNumberUI>().UpdateOrderNumberText(Order.colorStatus(child.GetComponent<OrderNumberUI>().color));
+        }
+    }
+}
