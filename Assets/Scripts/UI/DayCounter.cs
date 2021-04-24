bd7057392966fa51bfbaabea5cf31faf7db954cb
harrisonGardner <harrisongardner531@gmail.com>
2021-04-24 16:01:43 -0600

    Revert "Revert "Maybe Sounds?""
    This reverts commit 7f972f8f27662ec1c955eb2e3af36a280e6782b8.

--- Assets/Scripts/UI/DayCounter.cs
+++ Assets/Scripts/UI/DayCounter.cs
@@ -7,14 +7,5 @@ public class DayCounter : MonoBehaviour
 {
     public GameObject orderObject;
     public string deadline;
-
-    private void Start()
-    {
-        
-    }
-
-    //private void FixedUpdate()
-    //{
-    //    gameObject.GetComponent<Text>().text = $"Day: {MasterController.DayNumber}     deadline: {orderObject.GetComponent<Order>().maxNumDays}";
-    //}
+    public AudioSource dayStartDing;
 }
